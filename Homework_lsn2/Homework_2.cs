84883345f5fc96479019de05733779cba8b24bfe
Павел Ремизов <remizov@rightway.games>
2020-07-12 14:56:24 +0300

    выполнено второе задание

--- Homework_lsn2/Homework_2.cs
+++ Homework_lsn2/Homework_2.cs
@@ -13,6 +13,10 @@ namespace Homework_lsn2
         static void Main(string[] args)
         {
             Console.WriteLine(Converter(166));
+
+            Console.WriteLine(PowerA(3, 12));
+            Console.WriteLine(PowerB(3, 12));
+            Console.WriteLine(PowerC(3, 12));
             Console.ReadLine();
         }
 
@@ -20,5 +24,24 @@ namespace Homework_lsn2
         //рекурсию.
         static string Converter(int number) =>
             number >= 2 ? Converter(number / 2) + (number % 2).ToString() : number.ToString();
+
+        //Реализовать функцию возведения числа a в степень b:
+        //a.без рекурсии;
+        //b.рекурсивно;
+        //c. * рекурсивно, используя свойство четности степени.
+        static int PowerA(int number, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+                result *= number;
+            return result;
+        }
+        static int PowerB(int number, int power) =>
+            power >= 1 ? number * PowerB(number, --power) : 1;
+
+        static int PowerC(int number, int power) =>
+            power >= 1 ?
+            power % 2 == 0 ? PowerC(number * number, power / 2) : number * PowerC(number, --power) : 1;
+       
     }
 }
